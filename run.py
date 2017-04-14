f055df1fc227abefd3678f7a77ea7c5e373e1c23
prateek lalwani <prateeklalwani54@gmail.com>
2017-04-15 00:56:38 +0530

    Uploaded news script

---
+++ run.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import subprocess
+import time
+ 
+url = "http://timesofindia.indiatimes.com/"
+ 
+def open_url(url):
+    return requests.get(url).text  # returns html
+ 
+def get_bsoup_object(html):
+    return BeautifulSoup(html, "lxml")  # returns soup (BeautifulSoup's object)
+ 
+ 
+def sendmessage(message):
+    subprocess.Popen(['notify-send', message])
+    return
+ 
+ 
+def main():
+    myhtml = open_url(url)
+    soup = get_bsoup_object(myhtml)
+    j = 1
+   
+    for i in soup.find('ul',attrs={'class':'list9'}).findAll('li'):
+        #print(str(j) + " " + i.text)
+        sendmessage(str(j) + " " + i.text)
+        time.sleep(10)
+        j += 1
+       
+ 
+ 
+while True:
+    main()
+    time.sleep(3600)
